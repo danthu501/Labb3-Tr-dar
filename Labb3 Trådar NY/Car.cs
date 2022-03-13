90fd5a31eb4b1dfaf7558dd0eea60ee3da7f12a1
danthu501 <91268795+danthu501@users.noreply.github.com>
2022-03-13 17:34:04 +0100

    Add project files.

---
+++ Labb3 Trådar NY/Car.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Labb3_Trådar_NY
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public decimal Speed = 27.78m;
+        public decimal RacingDistance = 1000;
+        public decimal Distance = 0;
+        public bool Finished = false;
+
+
+
+        public void StartRaceToyota()
+        {
+            CarName = "Toyota";
+
+            int a = 0;
+            for (Distance = 0; Distance <= RacingDistance; Distance = Distance + Speed)
+            {
+
+                Thread.Sleep(1000);
+                a = a + 1;
+                if (a == 30)
+                {
+                    Problems(CarName);
+                    a = 0;
+                }
+
+
+                
+            }
+            Finished = true;
+            Finsish(CarName, Finished);
+        }
+
+        public void StartRaceVolvo()
+        {
+            CarName = "Volvo";
+            
+            int a = 0;
+            for (Distance = 0; Distance <= RacingDistance; Distance = Distance + Speed)
+            {
+
+                Thread.Sleep(1000);
+                a = a + 1;
+                if (a == 30)
+                {
+                    Problems(CarName);
+                    a = 0;
+                }
+            }
+            Finished = true;
+            Finsish(CarName, Finished);
+
+        }
+        public void OutOfGas(string Car)
+        {
+            Console.WriteLine(Car + " behöver tanka, står stilla i 30 skeunder");
+            Thread.Sleep(30000);
+        }
+
+        //2/50 = 4%
+        public void FlatTire(string Car)
+        {
+            Console.WriteLine("{0} behöver byta däck, står still i 20 sekunder", Car);
+            Thread.Sleep(20000);
+        }
+        //5/50 = 10%
+        public void BirdOnWindShield(string Car)
+        {
+            Console.WriteLine("{0} behöver tvättas, står still i 10 sekunder", Car);
+            Thread.Sleep(10000);
+        }
+        //10/50 = 20% 
+        public void EngineFaliure(string Car)
+        {
+            Console.WriteLine("{0} Har motorproblem, bilens hastighet sänks med 1 km/h", Car);
+            Speed= Speed - 0.2777778m;
+        }
+
+        public void Problems(string Carname)
+        {
+            int Test = new Random().Next(1, 51);
+            //Console.WriteLine(Test);
+
+            if (Test <= 1)
+            {
+                OutOfGas(Carname);
+            }
+            if (Test >= 49)
+            {
+                FlatTire(Carname);
+            }
+            if (Test >= 2 && Test < 5)
+            {
+                BirdOnWindShield(Carname);
+            }
+            if (Test >= 10 && Test < 20)
+            {
+                EngineFaliure(Carname);
+            }
+            else
+            {
+
+            }
+        }
+        public void Started(string CarName)
+        {
+            Console.WriteLine("{0} Har startat", CarName);
+        }
+
+        public void Finsish(string car, bool Finished)
+        {
+            if (Finished)
+            {
+                Console.WriteLine("{0} Bilen är i mål!", car);
+            }
+            
+            
+            
+
+            
+
+
+
+
+        }
+    }
+
+}
---
+++ Labb3 Trådar NY/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Labb3_Trådar_NY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car Car2 = new Car();
+            Car Info = new Car();
+
+
+            Thread th1 = new Thread(Car1.StartRaceVolvo);
+            Thread th2 = new Thread(Car2.StartRaceToyota);
+            
+
+            th1.Start();
+            th2.Start();
+
+            Console.WriteLine("För att se statusen i tävlingen tryck på enter");
+            WriteSatus(Car1, Car2);
+
+            FirstToFinish(Car1, Car2);
+
+
+  
+
+
+
+        }
+
+        public static void WriteSatus(Car car1, Car car2)
+        {
+            
+            while (!car1.Finished && !car2.Finished)
+            {
+                
+              
+
+                
+                if (Console.KeyAvailable)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        decimal speed1 = car1.Speed * 3.6m;
+                        string Message1 = (car1.CarName + " Har kört: " + Math.Round(car1.Distance) + " meter och har hastigheten: " + Math.Round(speed1) + " km/h");
+                        Console.WriteLine(Message1);
+                        decimal speed2 = car2.Speed * 3.6m;
+                        string Message2 = (car2.CarName + " Har kört: " + Math.Round(car2.Distance) + " meter och har hastigheten: " + Math.Round(speed2) + " km/h");
+                        Console.WriteLine(Message2);
+                    }
+                }
+            }
+
+        }
+        public static void FirstToFinish(Car car1, Car car2)
+        {
+            if (!car1.Finished && car2.Finished)
+            {
+                Console.WriteLine(car2.CarName + " är i mål och Vann");
+
+            }
+            if (car1.Finished && !car2.Finished)
+            {
+                Console.WriteLine(car1.CarName + " Vann");
+
+            }
+        } 
+
+    }
+}
